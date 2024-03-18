33071d6b067d85f9d2b42e244513af2ebc141890
helysson16 <hdfs1@aluno.ifal.edu.br>
2024-03-18 08:19:33 -0300

    Adiciona o Componente gato

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Gato from './gato';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello word, how are you now ?</Text>
+      <Gato/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ gato.js
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native';
+
+const Gato = () => {
+    return(
+    <View>
+        <Text>Meu nome é Sérgio</Text>
+    </View>
+    );
+}
+
+export default Gato;
